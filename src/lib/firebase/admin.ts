35f459a8e124c5ca362513c2682ff6e9da0bf423
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-07-23 15:37:31 +0800

    feat: add service account to env

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -2,13 +2,14 @@ import * as admin from "firebase-admin";
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
-import * as path from "path";
 
 if (getApps().length === 0) {
-  const serviceAccountPath = path.join(process.cwd(), "serviceAccount.json");
+  const serviceAccount = JSON.parse(
+    (process.env.FIREBASE_SERVICE_ACCOUNT || "{}").replace(/'/g, '"')
+  );
 
   initializeApp({
-    credential: admin.credential.cert(serviceAccountPath),
+    credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }

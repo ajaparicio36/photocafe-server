5205aa122ee18aee70635e3253e7eec887196ea4
AJ APARICIO <aparicio.anthonyjohn@gmail.com>
2025-07-23 18:44:37 +0800

    feat: update Firebase admin initialization to use service account file

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -2,14 +2,13 @@ import * as admin from "firebase-admin";
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
+import * as path from "path";
 
 if (getApps().length === 0) {
-  const serviceAccount = JSON.parse(
-    (process.env.FIREBASE_SERVICE_ACCOUNT || "{}").replace(/'/g, '"')
-  );
+  const serviceAccountPath = path.join(process.cwd(), "serviceAccount.json");
 
   initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccountPath),
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   });
 }
